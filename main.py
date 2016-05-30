11b0b743691c8ec16d84b00fbe3337ebe50261b4
kstanisz <staniszewski.kacper@gmail.com>
2016-05-30 12:56:29 +0200

    Read extreme points from gallery coordinates

--- main.py
+++ main.py
@@ -16,8 +16,25 @@ def read_data_from_file():
         gallery_coordinates = [[float(x) for x in line.split()] for line in file]
     return radius, net_density, gallery_coordinates
 
+def read_extremes_from_coordinates(gallery_coordinates):
+    min_x=gallery_coordinates[0][0]
+    max_x=gallery_coordinates[0][0]
+    min_y=gallery_coordinates[0][1]
+    max_y=gallery_coordinates[0][1]
+    for i in range(1,len(gallery_coordinates)):
+        if gallery_coordinates[i][0] < min_x:
+            min_x=gallery_coordinates[i][0]
+        if gallery_coordinates[i][0] > max_x:
+            max_x=gallery_coordinates[i][0]
+        if gallery_coordinates[i][1] < min_y:
+            min_y = gallery_coordinates[i][1]
+        if gallery_coordinates[i][1] > max_y:
+            max_y = gallery_coordinates[i][1]
+    return min_x,max_x,min_y,max_y
+
 def main():
     radius, net_density, gallery_coordinates = read_data_from_file()
+    min_x, max_x, min_y, max_y=read_extremes_from_coordinates(gallery_coordinates)
     gallery_polygon = Polygon(gallery_coordinates)
     print("Pole całkowite galerii: {}".format(gallery_polygon.area))
 
